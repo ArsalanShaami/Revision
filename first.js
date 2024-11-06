cccd8ff97337cb8238d7d1308af54ec1e68874eb
Arsalan Shaami <iftars2774@gmail.com>
2024-11-06 22:54:35 +0530

    my first push

---
+++ first.js
@@ -0,0 +1,62 @@
+// console.log('Hello')
+
+// let ar = [2,3,9,15,8]
+
+// console.log(ar.forEach((n)
+
+// ))
+
+// console.log(
+//     ar.filter(
+//         (x) => x%2==0
+//     )
+// )
+
+
+// C = [0, 38, 6, -6]
+
+// console.log(
+//     C.map(
+//         (x) => ((x*9)/5) + 32
+//     )
+// )
+
+// ar = [12,4,23,12,2,1,2,4,45,45,56,34,3,24,23,1]
+// let arr = ar.sort((a,b)=> b-a)
+// console.log(ar
+// )
+
+
+ar = [50,40, 10, 6, 59, 8]
+
+// arr = ar.sort((a,b)=> b-a)
+// console.log(arr[0])
+// console.log(ar[0])
+
+// function Great(x,y) {
+//     if(x>y){
+//         return x
+//     }else{
+//         return y
+//     }
+// }
+// // console.log(Great(18,15))
+
+
+// arr = ar.reduce(
+//     Great
+// )
+
+// console.log(arr)
+
+
+
+
+
+
+
+
+
+
+
+
